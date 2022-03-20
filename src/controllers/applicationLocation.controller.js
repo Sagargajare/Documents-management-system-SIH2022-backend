9f5aaa93138096f4160640a677a8e706932a847c
SurabhiMore <surabhi.pmore@gmail.com>
2022-03-20 14:34:04 +0530

    CRUD for applicationLocation

---
+++ src/controllers/applicationLocation.controller.js
@@ -0,0 +1,32 @@
+const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
+const { applicationLocationService } = require('../services');
+
+const allApplicationLocations = catchAsync(async (req, res) => {
+  const applicationLocations = await applicationLocationService.getAllApplicationLocation();
+  res.status(httpStatus.OK).send({ data: applicationLocations });
+});
+const getApplicationLocation = catchAsync(async (req, res) => {
+  const applicationLocation = await applicationLocationService.getApplicationLocationById(req.params.id);
+  res.status(httpStatus.OK).send({ data: applicationLocation });
+});
+const createApplicationLocation = catchAsync(async (req, res) => {
+  const applicationLocation = await applicationLocationService.createApplicationLocation(req.body);
+  res.status(httpStatus.OK).send({ data: applicationLocation });
+});
+const deleteApplicationLocation = catchAsync(async (req, res) => {
+  const applicationLocation = await applicationLocationService.deleteApplicationLocation(req.params.id);
+  res.status(httpStatus.OK).send({ data: applicationLocation });
+});
+const updateApplicationLocation = catchAsync(async (req, res) => {
+  const applicationLocation = await applicationLocationService.updateApplicationLocation(req.params.id, req.body);
+  res.status(httpStatus.OK).send({ data: applicationLocation });
+});
+
+module.exports = {
+  allApplicationLocations,
+  getApplicationLocation,
+  createApplicationLocation,
+  updateApplicationLocation,
+  deleteApplicationLocation,
+};
