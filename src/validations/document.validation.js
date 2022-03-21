a2e46701ba7418f7bf5e87b160fa5ad500885251
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2022-03-21 22:56:04 +0530

    validation fix

--- src/validations/document.validation.js
+++ src/validations/document.validation.js
@@ -4,7 +4,6 @@ const create = {
   body: Joi.object().keys({
     type: Joi.string().required().valid('ResidenceVerification', 'CasteVerification', 'DisabilityVerification'),
     note: Joi.string().required(),
-    _id: Joi.object().required(),
   }),
 };
 
--- src/validations/request.validation.js
+++ src/validations/request.validation.js
@@ -1,11 +1,12 @@
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const create = {
   body: Joi.object().keys({
     status: Joi.string().required().valid('reject', 'in-process', 'approved'),
     comment: Joi.string().required(),
-    application: Joi.object().required(),
-    sentTo: Joi.object().required(),
+    application: Joi.string().guid(),
+    sentTo: Joi.objectId(),
   }),
 };
 
